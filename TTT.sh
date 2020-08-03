bab597bde3e0afb3feea260b4bfd32ce2d09f8ff
Sangita <pal07sangita@gmail.com>
2020-08-03 10:48:08 +0530

    AddedUC1:resetting the board

---
+++ TTT.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+reset(){
+  echo "====================="
+  echo "Created by Sangita-Pal"
+  echo "2020.8.3"
+  echo "TicTacToe game in bash"
+
+  Arr=(. . . . . . . . .)
+  player=1
+  gamestatus=1
+  echo "====================="
+  echo "Game started"
+}
+reset
