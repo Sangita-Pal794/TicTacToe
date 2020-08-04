1f2fdb6e0e5f453bf7c9e6e827d8b34a83b571b1
Sangita <pal07sangita@gmail.com>
2020-08-04 11:20:20 +0530

    Added UC5:Displaying the boardfor Player one

---
+++ TTT.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+print(){
+  echo "r\c 0 1 2"
+  echo "0   ${Arr[0]} ${Arr[1]} ${Arr[2]}"
+  echo "1   ${Arr[3]} ${Arr[4]} ${Arr[5]}"
+  echo "2   ${Arr[6]} ${Arr[7]} ${Arr[8]}"
+}
+print
