88e13379fb65bfeafbba01aeffa8cae9d926b56f
Sangita <pal07sangita@gmail.com>
2020-08-04 22:42:39 +0530

    Added UC4:Display the board for user

---
+++ TTT.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Choose letter X/O as an user if my turn's comes
+declare -a GRID=('_' '_' '_' '_' '_' '_' '_' '_' '_')
+for TURN in {0..8}
+do
+   display_GRID
+  if [ $(($TURN%2)) -eq 0 ]
+  then
+       echo X\'s TURN
+ else
+	echo O\'s TURN
+ fi
+done
+
+
+display_GRID(){
+  echo ${GRID[0]}$'\t'${GRID[1]}$'\t'${GRID[2]}$'\t'$'\n'
+  echo ${GRID[3]}$'\t'${GRID[4]}$'\t'${GRID[5]}$'\t'$'\n'
+  echo ${GRID[6]}$'\t'${GRID[7]}$'\t'${GRID[8]}$'\t'$'\n'
+}
